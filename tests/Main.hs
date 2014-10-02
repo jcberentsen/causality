f43cfd45eac3358f89e430462bd8c3757d4e9f6a
Jarl Christian Berentsen <jc.berentsen@gmail.com>
2014-10-02 11:51:34 +0200

    Warnings as errors, suppress signature warning in test suite

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,7 +1,19 @@
+{-# LANGUAGE TemplateHaskell  #-}
+{-# OPTIONS_GHC -fno-warn-missing-signatures  #-}
+
 import Test.Tasty
 --import Test.Tasty.SmallCheck as SC
 --import Test.Tasty.QuickCheck as QC
 import Test.Tasty.HUnit
+import Test.Tasty.TH
+
+isFact :: Bool
+isFact = True
+
+myTestGroup :: TestTree
+myTestGroup = $(testGroupGenerator)
+
+premise_implies_conclusion = testCase "premise implies conclusion" $ True @?= isFact
 
 main :: IO ()
 main = defaultMain tests
@@ -12,4 +24,5 @@ tests = testGroup "Tests" [unitTests]
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
     [ testCase "sanity" $ True @?= True
+    , myTestGroup
     ]
