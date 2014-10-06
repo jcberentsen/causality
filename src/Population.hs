ceabea1b898193afcbb8cc35d04b113c848ee88d
Jarl Christian Berentsen <jc.berentsen@gmail.com>
2014-10-06 14:27:21 +0200

    Population sampling Causally

--- src/Population.hs
+++ src/Population.hs
@@ -6,5 +6,11 @@ module Population where
 import Model
 import Evidence
 
-sample :: (Eq name, Eq p) => p -> CausalModel name p -> [Evidence name p]
-sample _seed _model = []
+sample :: (Eq name, Eq p, Truthy p) => p -> CausalModel name p -> [Evidence name p]
+sample seed model = case model of
+    Ignorance -> []
+    Evidently e -> e
+    Causally cause effect -> eval_causalmodel model synthetic_evidence
+        where
+            synthetic_evidence = [if seed == yes then cause else void cause]
+    _ -> []
--- tests/PopulationTests.hs
+++ tests/PopulationTests.hs
@@ -17,9 +17,23 @@ population_test_group = $(testGroupGenerator)
 prop_ignorant_model_generates_no_population seed = Population.sample seed model == []
     where model = Ignorance :: CausalModel Bool Bool
 
-prop_fact_model_always_generates_fact seed = Population.sample seed fact_model == [ a_fact ]
+prop_evidently_model_always_generates_fact seed = Population.sample seed fact_model == [ a_fact ]
     where fact_model = Evidently [a_fact]
           a_fact = fact "a fact" :: Evidence String Bool
+
+prop_sampling_cause_generates_cause_and_effect seed
+    = seed == yes ==>
+        Population.sample seed causal_model == [ cause, effect ]
+    where causal_model = Causally cause effect
+          cause = fact "cause" :: Evidence String Bool
+          effect = fact "effect" :: Evidence String Bool
+
+prop_sampling_cause_with_false_evidence_yields_counterfacts seed
+    = seed == no ==>
+        Population.sample seed causal_model == [ void cause, void effect ]
+    where causal_model = Causally cause effect
+          cause = fact "cause" :: Evidence String Bool
+          effect = fact "effect" :: Evidence String Bool
 
 -- Populations. A causal system is a generator of populations. The input is generators of facts.
 -- The output is population of facts.
