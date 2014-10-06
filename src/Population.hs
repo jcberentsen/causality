ae0f5d2f7580e36ae5ad17843fb1470bb80e3bdc
Jarl Christian Berentsen <jc.berentsen@gmail.com>
2014-10-06 16:31:41 +0200

    Sampling a CausalModel is about evaluating on synthethic evidence

--- src/Population.hs
+++ src/Population.hs
@@ -8,13 +8,13 @@ import Evidence
 import Likelyhood
 
 sample :: (Eq name, Eq p, Truthy p) => p -> CausalModel name p -> [Evidence name p]
-sample seed model = case model of
-    Ignorance -> []
-    Evidently e -> e
-    Causally cause effect -> eval_causalmodel model synthetic_evidence
-        where
-            synthetic_evidence = [if seed == yes then cause else void cause]
-    _ -> []
+sample seed model =
+    eval_causalmodel model synthetic_evidence
+    where
+        synthetic_evidence =
+            case model of
+                Causally cause e -> [if seed == yes then cause else void cause]
+                _ -> []
 
 select :: (Ord like, Eq name, Eq like, Eq r, Truthy r) => Likelyhood name like -> like -> Evidence name r
 select (Likelyhood name (P like)) reality = Evidence name (P (if reality <= like then yes else no))
