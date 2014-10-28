4200ae1ab4e2dd4fc9213bac25a11b8f3aa33776
Jarl Christian Berentsen <jc.berentsen@gmail.com>
2014-10-28 16:08:41 +0100

    Use IsString for free String type

--- src/Examples/DihybridCross.hs
+++ src/Examples/DihybridCross.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE TemplateHaskell  #-}
+{-# LANGUAGE TemplateHaskell, OverloadedStrings  #-}
 {-# OPTIONS_GHC -fno-warn-missing-signatures -fno-warn-orphans #-}
 
 module Examples.DihybridCross where
+
+import Data.String
 
 import Evidence
 import Model
@@ -25,12 +27,15 @@ import Model
 -- BbGg     BbGg      BBGG, BbGg, etc...
 
 -- Allele facts
+allele_G, allele_g, allele_B, allele_b :: IsString name => Evidence name Bool
 allele_G = truly "G"
 allele_g = truly "g"
 allele_B = truly "B"
 allele_b = truly "b"
 
 -- Positional alleles in genes
+allele_G1, allele_g1, allele_B1, allele_b1, allele_G2, allele_g2, allele_B2, allele_b2 :: IsString name => Evidence name Bool
+
 allele_G1 = truly "G1"
 allele_G2 = truly "G2"
 
@@ -44,18 +49,29 @@ allele_b1 = truly "b1"
 allele_b2 = truly "b2"
 
 -- derived facts from positional alleles
+cause_G :: IsString name => CausalModel name Bool
 cause_G = AnyCause [allele_G1, allele_G2] allele_G
 
 -- Phenotype facts
+blue, green, brown :: IsString name => Evidence name Bool
 blue = truly "blue"
 green = truly "green"
-brown = truly "brown" :: Evidence String Bool
+brown = truly "brown"
 
 -- Gene causes Phenotype
+devo_brown :: IsString name => CausalModel name Bool
 devo_brown = AnyCause [allele_B] brown
 
 -- green devo when 'all b' alleles (ie not any B) and any 'G'
+cause_bb :: IsString name => CausalModel name Bool
 cause_bb = AllCause [allele_b1, allele_b2] both_bb
+
+both_bb :: IsString name => Evidence name Bool
 both_bb = truly "bb"
-cause_green = AllCause [allele_G, both_bb] green :: CausalModel String Bool
+
+cause_green :: IsString name => CausalModel name Bool
+cause_green = AllCause [allele_G, both_bb] green
+
+devo :: IsString name => CausalModel name Bool
+devo = Multiple [devo_brown, cause_G, cause_bb, cause_green]
 
--- tests/DihybridCrossTests.hs
+++ tests/DihybridCrossTests.hs
@@ -11,13 +11,14 @@ import Test.Tasty.QuickCheck as QC
 import Examples.DihybridCross
 
 import Model
+import Evidence
 
 import qualified Data.Set as Set
 
 dihybrid_test_group = $(testGroupGenerator)
 
 prop_required_cause_for_green_phenotype =
-    Set.member green $ eval_causalmodel [allele_G, both_bb] cause_green
+    Set.member green $ eval_causalmodel [allele_G :: Evidence String Bool, both_bb] cause_green
 
 prop_only_G_allele_not_enough_to_conclude_green_phenotype =
-    not (Set.member green $ eval_causalmodel [allele_G] cause_green)
+    not (Set.member green $ eval_causalmodel [allele_G :: Evidence String Bool] cause_green)
